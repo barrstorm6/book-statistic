52d4c1facb17d3e7c93d0f8360b0b706ecdc796e
barrstorm6 <barrstorm6@gmail.com>
2022-07-18 16:50:36 +0600

    init commit

---
+++ src/main/java/edu/junit5/bookstat/BookStatisticApp.java
@@ -0,0 +1,13 @@
+package edu.junit5.bookstat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookStatisticApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookStatisticApp.class, args);
+	}
+
+}
---
+++ src/test/java/edu/junit5/bookstat/BookStatisticAppTests.java
@@ -0,0 +1,13 @@
+package edu.junit5.bookstat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStatisticAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
